cd39125ca8337e4992a1556f0bc50f0862da7209
chandrashekhar singh <synthesis_chandrashekhar@hotmail.com>
2019-07-08 14:30:01 +0530

    Add project files.

---
+++ UnitTestDemo.Tests/Controllers/HomeControllerTest.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Web.Mvc;
+using UnitTestDemo.Controllers;
+using UnitTestDemo.Models;
+
+namespace UnitTestDemo.Tests.Controllers
+{
+    [TestClass]
+    public class HomeControllerTest
+    {
+        [TestMethod]
+        public void Index()
+        {
+            // Arrange
+            HomeController controller = new HomeController();
+
+            // Act
+            ViewResult result = controller.Index() as ViewResult;
+
+            // Assert
+            Assert.IsNotNull(result);
+        }
+
+        [TestMethod]
+        public void About()
+        {
+            // Arrange
+            HomeController controller = new HomeController();
+
+            // Act
+            ViewResult result = controller.About() as ViewResult;
+
+            // Assert
+            Assert.AreEqual("Your application description page..", result.ViewBag.Message);
+        }
+
+        [TestMethod]
+        public void Contact()
+        {
+            // Arrange
+            HomeController controller = new HomeController();
+
+            // Act
+            ViewResult result = controller.Contact() as ViewResult;
+
+            // Assert
+            Assert.IsNotNull(result);
+        }
+
+        [TestMethod]
+        public void TestIndexMethod()
+        {
+            // Arrange
+            HomeController controller = new HomeController();
+            string ViewName = "Index";
+            string name = "Chandu";
+            // Act
+            var result =controller.Index() as ViewResult;
+            var model = (Employee)result.Model;
+
+            // Assert
+            Assert.AreEqual(ViewName, result.ViewName);
+            Assert.AreEqual(name,model.Name);
+        }
+    }
+}
---
+++ UnitTestDemo/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using UnitTestDemo.Models;
+
+namespace UnitTestDemo.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            Employee emp = new Employee()
+            {
+                id = 1,
+                Name="Chandu"
+
+            };
+
+
+            return View("Index",emp);
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page..";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
